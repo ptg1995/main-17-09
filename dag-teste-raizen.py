e28028c538a6ed3d8377c2298795d0bff5083088
Philip Guedes <philiptavaresvs@gmail.com>
2023-09-17 18:01:40 -0300

    Criação das tabelas fim

--- dag-teste-raizen.py
+++ dag-teste-raizen.py
@@ -1,15 +1,19 @@
 # datetime
 from datetime import timedelta, datetime
 import os
+
 # The DAG object
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from teste_raizen.get_data import extract_data_from_git
+from teste_raizen.save_data_silver_raw import save_tables_silver_raw
 
 ##variaveis
 github_url = "https://github.com/ptg1995/data-engineering-test/raw/master/assets/vendas-combustiveis-m3.xls"
 output_filename = f"data_full.xls"
+path_data_full_bronze = "/root/airflow/dags/bronze_raw/data_full.xlsx"
+sheets_data_full = {"DPCache_m3": "oil_derivative", "DPCache_m3_2": "diesel"}
 
 ##DAG
 with DAG(
@@ -23,14 +27,20 @@ with DAG(
     get_data_xls = PythonOperator(
         task_id="get_data_xls",
         python_callable=lambda: extract_data_from_git(
-            github_url,os.path.join(os.getcwd(),"dags" ,"bronze_raw", output_filename)
+            github_url, os.path.join(os.getcwd(), "dags", "bronze_raw", output_filename)
         ),
         dag=dag,
     )
-    # transform_file_xlsto_xlsx = BashOperator(
-    #     task_id="run_after_loop",
-    #     bash_command="libreoffice --headless --invisible --convert-to xlsx ./teste_raizen/outputs/temp1.xls --outdir ./teste_raizen/outputs/"
-    # )
-
+    transform_file_xlsto_xlsx = BashOperator(
+        task_id="transform_file_xlsto_xlsx",
+        bash_command="libreoffice --headless --invisible --convert-to xlsx /root/airflow/dags/bronze_raw/data_full.xls --outdir /root/airflow/dags/bronze_raw",
+    )
+    save_tables_silver_raw_task = PythonOperator(
+        task_id="save_tables_silver_raw_task",
+        python_callable=lambda: save_tables_silver_raw(
+            path_data_full_bronze, sheets_data_full
+        ),
+        dag=dag,
+    )
 # Execução das tasks
-get_data_xls
+get_data_xls >> transform_file_xlsto_xlsx >> save_tables_silver_raw_task
